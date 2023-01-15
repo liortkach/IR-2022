17fe7fcea6bacb570433c5adaf8f0720ac6c5ab9
Denis <55393990+denisrize@users.noreply.github.com>
2023-01-15 16:39:11 +0200

    Update search_frontend.py

--- search_frontend.py
+++ search_frontend.py
@@ -71,7 +71,7 @@ class MyFlaskApp(Flask):
 
         self.BM25_body = BM25(self.body_stem_index, self.DL_body, "_body_stem", app.page_rank, k1=1.5, b=0.6)
         self.BM25_title = BM25(self.title_stem_index, self.DL_title,  "_title_stem", app.page_rank, k1=2.2, b=0.85)
-        self.cosine_body = CosineSim(self.index_body, self.DL_body, "_body_stem", app.page_rank, self.doc_norm)
+        self.cosine_body = CosineSim(self.index_body, self.DL_body, "", app.page_rank, self.doc_norm)
         #self.cosine_title = CosineSim(self.title_index, self.DL_title, "_title_stem", app.page_rank, self.doc_norm)
 
         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, **options)
@@ -136,7 +136,7 @@ def search_body():
       return jsonify(res)
     # BEGIN SOLUTION
     res = []
-    res_list = app.cosine_body.calcCosineSim(app.tokenizer.tokenize(query, False), app.index_body, app.DL_body, N=100)
+    res_list = app.cosine_body.calcCosineSim(app.tokenizer.tokenize(query, False), app.DL_body, N=100)
     res_list = [(doc_id, app.doc_title_dict[doc_id]) for doc_id, score in res_list]
     res = res_list
     # END SOLUTION
